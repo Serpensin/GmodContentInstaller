a8d0ec11f33f4767e9cfd0968c9615285db0bca7
Serpensin <wissmann.matti@gmail.com>
2025-06-27 16:09:22 +0200

    Update assembly file version to 1.0.1
    Incremented the `AssemblyFileVersion` from "1.0.0" to "1.0.1" to reflect a minor version change.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.Versioning;
 
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.1")]
 [assembly: AssemblyCompany("SerpentModding")]
 [assembly: AssemblyProduct("GMod Content Wizard")]
 [assembly: AssemblyTitle("GModContentWizard")]
