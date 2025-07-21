21c01a0034d8a6c1c384b6e3b2eb777c9dc99dd3
Serpensin <wissmann.matti@gmail.com>
2025-07-21 21:28:59 +0200

    Update assembly versioning information
    Removed `AssemblyVersion` attribute and updated `AssemblyFileVersion` from "1.0.1" to "1.1.0" to reflect new features and improvements.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,8 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0")]
 [assembly: AssemblyCompany("SerpentModding")]
 [assembly: AssemblyProduct("GMod Content Wizard")]
 [assembly: AssemblyTitle("GModContentWizard")]
