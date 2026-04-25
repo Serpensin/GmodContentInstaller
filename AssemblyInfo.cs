1ddfbee24185fe64f66f04b9515fc065306e23e1
Serpensin <wissmann.matti@gmail.com>
2026-04-25 21:31:47 +0200

    Bump version to 2.0.1.0 in AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("GMod Content Wizard")]
 [assembly: AssemblyDescription("Install Garry's Mod content like maps and models")]
 [assembly: AssemblyCopyright("Copyright 2026 Serpensin")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
